d6cb671d60080e1cd45a6a96b11c56ed6153c645
Abigail Slovick <abby.slovick@gmail.com>
2018-08-09 20:39:59 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -13,19 +13,68 @@ first_recycle_bin =  {
   "latitude"=>"40.890848989",
   "park_site_name"=>"227th St. Plgd"
 }
-
+second_recycle_bin = { 
+  "address"=>"Allerton Ave & Moshulu Pkway",
+  "site_type"=>"Subproperty",
+  "longitude"=>"-73.8771283938",
+  "borough"=>"Bronx",
+  "latitude"=>"40.8488907878",
+  "park_site_name"=>"Allerton Ballfields"
+}
+ 
+third_recycle_bin = {
+  "address"=>"Arthur Ave & 187 St",
+  "site_type"=>"Outdoor",
+  "longitude"=>"-73.887564999999995",
+  "borough"=>"Bronx",
+  "latitude"=>"40.85557",
+  "park_site_name"=>"Arthur Ave & E 187 St"
+}
+ 
+fourth_recycle_bin = {
+  "address"=>"895 Shore Road, Pelham Bay Park",
+  "site_type"=>"Outdoor ",
+  "longitude"=>"-73.805549",
+  "borough"=>"Bronx",
+  "latitude"=>"40.871864",
+  "park_site_name"=>"Barstow Mansion"
+}
 
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
 def address(first_recycle_bin)
-  
   first_recycle_bin["address"]
 end  
+
+def address(second_recycle_bin)
+  second_recycle_bin["address"]
+end 
+
+def address(third_recycle_bin)
+  third_recycle_bin["address"]
+end 
+
+def address(fourth_recycle_bin)
+  fourth_recycle_bin["address"]
+end 
+
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
 
 def borough(first_recycle_bin)
   first_recycle_bin["borough"]
+end 
+
+def borough(second_recycle_bin)
+  second_recycle_bin["borough"]
+end 
+
+def borough(third_recycle_bin)
+  third_recycle_bin["borough"]
+end 
+
+def borough(fourth_recycle_bin)
+  fourth_recycle_bin["borough"]
 end 
 
 #3. Define a method called `all_keys` that takes a hash as an argument.
@@ -34,6 +83,24 @@ end
 
 def all_keys(first_recycle_bin)
   first_recycle_bin.each do |key, value|
+    puts "#{key}"
+  end
+end 
+
+def all_keys(second_recycle_bin)
+  second_recycle_bin.each do |key, value|
+    puts "#{key}"
+  end
+end 
+
+def all_keys(third_recycle_bin)
+  third_recycle_bin.each do |key, value|
+    puts "#{key}"
+  end
+end 
+
+def all_keys(fourth_recycle_bin)
+  fourth_recycle_bin.each do |key, value|
     puts "#{key}"
   end
 end 
@@ -48,12 +115,48 @@ def all_data(first_recycle_bin)
   end
 end 
 
+def all_data(second_recycle_bin)
+  second_recycle_bin.each do |key, value|
+    puts "#{value}"
+  end
+end 
+
+def all_data(third_recycle_bin)
+  third_recycle_bin.each do |key, value|
+    puts "#{value}"
+  end
+end 
+
+def all_data(fourth_recycle_bin)
+  fourth_recycle_bin.each do |key, value|
+    puts "#{value}"
+  end
+end 
+
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
 
 def key_and_data(first_recycle_bin)
   first_recycle_bin.each do |key, value|
+    puts "The #{key} is #{value}"
+  end
+end 
+
+def key_and_data(second_recycle_bin)
+  second_recycle_bin.each do |key, value|
+    puts "The #{key} is #{value}"
+  end
+end 
+
+def key_and_data(third_recycle_bin)
+  third_recycle_bin.each do |key, value|
+    puts "The #{key} is #{value}"
+  end
+end 
+
+def key_and_data(fourth_recycle_bin)
+  fourth_recycle_bin.each do |key, value|
     puts "The #{key} is #{value}"
   end
 end 
